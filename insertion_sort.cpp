631f85333829ca787a3f888ca7a7e3bd42bcd578
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-09-23 11:26:51 -0600

    Añadir estadísticas al ordenamiento por inserción
    Se añadió una función `ordenamientoInsercion` para visualizar cada paso del algoritmo, mostrar el arreglo tras cada inserción y registrar comparaciones, desplazamientos e inserciones realizadas durante la ordenación.

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -11,6 +11,51 @@ void mostrarArreglo(int numeros[], int n)
     cout << endl;
 }
 
+void ordenamientoInsercion(int numeros[], int n)
+{
+    int comparaciones = 0;
+    int desplazamientos = 0;
+    int inserciones = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        int clave = numeros[i];
+        int j = i - 1;
+
+        cout << "\nInsertando: " << clave << endl;
+
+       
+        while (j >= 0)
+        {
+            comparaciones++;
+            if (numeros[j] > clave)
+            {
+                numeros[j + 1] = numeros[j];
+                desplazamientos++;
+                j--;
+            }
+            else
+            {
+                break; 
+            }
+        }
+
+        numeros[j + 1] = clave;
+        inserciones++;
+
+        
+        mostrarArreglo(numeros, n);
+    }
+
+   
+    cout << "\n========================================" << endl;
+    cout << "       ESTADISTICAS" << endl;
+    cout << "========================================" << endl;
+    cout << "Comparaciones:   " << comparaciones << endl;
+    cout << "Desplazamientos: " << desplazamientos << endl;
+    cout << "Inserciones:     " << inserciones << endl;
+}
+
 int main()
 {
     int numeros[] = {7, 3, 8, 2, 6, 4, 5};
